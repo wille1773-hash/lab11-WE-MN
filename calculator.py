cecf98eb3b9c2ad62582d089e53626b540e5fa0f
matty-glitch <mjnunes912@gmail.com>
2025-11-13 15:39:39 -0500

    merged changes

--- calculator.py
+++ calculator.py
@@ -15,12 +15,8 @@ def sub(a, b):
     return a - b
 def mul(a, b):
     return a * b
-def div(a, b):
-    if a == 0:
-        raise ZeroDivisionError("division by zero")
-    return ((b) / (a))
 def log(a, b):
-    if a <= 0 or a == 1 or b <= 0:
+    if a <= 0 or a == 1  or b <= 0:
         raise ValueError("invalid base or argument for logarithm")
     return math.log(b, a)
 def exp(a, b):
